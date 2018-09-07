838177ae9bb8b37b1024ade867925676b2008079
Alex Mastin <cd3770rn@go.minneapolis.edu>
2018-09-06 20:29:44 -0500

    Updated war.py

---
+++ war.py
@@ -0,0 +1,6 @@
+# The deck is divided down the middle to both players (26 cards each)
+# Each player removes a card from the top of their deck. This is called a battle.
+# The player with the higher card wins the battle and takes both cards.
+# If the cards are of the same face value, both players continue to draw until a player loses the battle.
+# The cards that are won are placed at the bottom of the deck.
+
