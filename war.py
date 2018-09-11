f2c0eac2876c55fe33d6cfaeec271a9628f386e1
Alex Mastin <cd3770rn@go.minneapolis.edu>
2018-09-11 12:58:09 -0500

    Updated war.py

--- war.py
+++ war.py
@@ -4,17 +4,17 @@
 # If the cards are of the same face value, both players continue to draw until a player loses the battle.
 # The cards that are won are placed at the bottom of the deck.
 
-# TODO: Make a dictionary to store the deck.
+# TODO: Make a dictionary to store the deck. -- Kenya
 
 # TODO: Create a makeDeck() function which creates and initializes the playing deck.
 # TODO: Create a draw() function which draws from the top of each player's deck
 # TODO: Create a discard() function which puts the cards at the bottom of the player's deck.
 # TODO: Create battle(), war() functions which perform the expected action
 
-# TODO: Create a Card class
+# TODO: Create a Card class -- Kenya
     # Each card should have a suit
     # Each card should have a value (A, 2, 3, etc.)
-# TODO: Create a Suit class
+# TODO: Create a Suit class -- Kenya
 
 
 # If there's more to do, divvy it up evenly?
