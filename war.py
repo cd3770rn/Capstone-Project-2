1e27d21bd14754882c2c6dccd51defc50216c8fd
Kenya <email@email.com>
2018-09-13 17:13:49 -0500

    Updated war.py

--- war.py
+++ war.py
@@ -6,60 +6,6 @@ import random
 # If the cards are of the same face value, both players continue to draw until a player loses the battle.
 # The cards that are won are placed at the bottom of the deck.
 
-# ==============================================
-# Kenya - I apologize for making it look like I did
-# all of the work. That wasn't my intention. I just
-# started coding and the next thing I knew I had done
-# everything that there was to do. Since you originally
-# created the object oriented side of things, I'm more
-# than willing to credit you for that, even though I
-# rearranged and reworked the bulk of it.
-#
-# The game is finished. In my testing, I did find
-# a bit of an interesting quirk with War, and in doing
-# so I found a way where you can distinguish credit.
-#
-# First, I would make a new branch for this version.
-# It's not COMPLETELY necessary but it's easier to work
-# that way. Don't worry if you can't figure it out, it's
-# not a huge deal.
-#
-# Some instances of War can run (seemingly) forever.
-# I have tested the code extensively, and had a game where
-# it ran for over 2 million turns -- AND IT'S RUNNING CORRECTLY!
-# So, what I think would be a good challenge for you is to
-# try to recreate the 'Instant War' variation as shown
-# on Wikipedia.
-#
-# It works as follows:
-# Any card that loses a battle is dead, or eliminated from the game.
-# The card that wins returns to the original owner. (Let's go with
-# 'whoever currently owns the card' instead of the original owner,
-# just for the sake of simplicity).
-# When cards tie, only one card is played by each player in the war.
-# A draw is possible, and gameplay is much quicker.
-#
-# Take the code here and make the changes required for the 'Instant War'
-# variation. If you can't get it working, that's fine. What we have here
-# is fully functional.
-#
-# The code to modify is inside of war().
-# TODO: card1 and card2 inside of war() should return to the respective player instead of going into the stack.
-# TODO: Each player should only draw a single card. Remove the code which has them draw a second card.
-# TODO: If a card loses, the player should discard it. Cards should NOT go to the stack. [ex. player1.discard(card)]
-# TODO: If a card wins, it should return to the player. The winner should NOT receive the contents of the stack. [ex. hand1.update({str(card) : card})]
-# TODO: If during a war, the drawn cards are a tie, they should return to their respective player.
-# TODO: Update print() statements to display the correct output. [ex. If a tie occurs during a war, display "The war ends in a draw."]
-
-# ==============================================
-# Alex - I made a new branch so I can try and create the "instant war" game without messing up the code in the master.
-# But class is already tomorrow and I have two other classes I really need to work on tonight
-# so I don't even know if/when I'll have time to try and do the instant war game.
-# Plus it's hard to like take someone else's code, which in my eyes is advanced, and then try to modify it.
-# We'll talk more in class about what we're going to do but...
-# I'm really feeling like I shouldn't take any credit for this. You're just too good lol
-# ==============================================
-
 
 class Card:
     # Initializes the card with a suit and rank (value)
